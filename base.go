c26d74db347809bda73ad204fccabce73c5f71e9
quocdaijr <quocdaijr@gmail.com>
2024-01-06 11:10:43 +0700

    push fun code

--- base.go
+++ base.go
@@ -3,7 +3,7 @@ package your_age
 import "time"
 
 func CalculateAgeFromBirthDay(birthDay string) int {
-	birthDayDatetime, _ := time.Parse("2006-01-02", date)
+	birthDayDatetime, _ := time.Parse("2006-01-02", birthDay)
 	return time.Now().Year() - birthDayDatetime.Year()
 }
 
